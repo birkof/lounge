8fd0ab9a4568f8994e149c0f074335cc2decb5a6
Pavel Djundik <github@xpaw.me>
2017-12-01 20:04:50 +0200

    Handle javascript errors while loading

--- client/js/loading-slow-alert.js
+++ client/js/loading-slow-alert.js
@@ -19,3 +19,25 @@ setTimeout(function() {
 document.getElementById("loading-slow-reload").addEventListener("click", function() {
 	location.reload();
 });
+
+if ("ErrorEvent" in window) {
+	window.g_LoungeErrorHandler = function LoungeErrorHandler(error) {
+		var title = document.getElementById("loading-title");
+		title.textContent = "An error has occured";
+
+		var element = document.createElement("p");
+		element.textContent = "An error has occured that prevented the client from loading correctly.";
+		title.parentNode.appendChild(element);
+
+		element = document.createElement("p");
+		element.textContent = "Open developer tools in your browser and check javascript console for more information.";
+		title.parentNode.appendChild(element);
+
+		element = document.createElement("p");
+		element.contentEditable = true;
+		element.textContent = error.message;
+		title.parentNode.appendChild(element);
+	};
+
+	window.addEventListener("error", window.g_LoungeErrorHandler);
+}
